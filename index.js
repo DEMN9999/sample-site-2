eec92bf19f904c3dbcf268f97aff25cd525bddc0
DEMN9999 <demn729999@gmail.com>
2023-11-28 21:22:56 +0500

    animation added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // const hide=()=>{let btn1=document.getElementsByTagName(`div`)[0];btn1.style.display=(`none`);document.body.style.overflowY=(`auto`)};
 window.addEventListener('scroll', function () {let nav = document.querySelector('nav');nav.classList.toggle(`stickey`, window.scrollY > 240)});
-let a=document.querySelector(`section`);
-let htmlBar=a.querySelectorAll(`div`)[13];
-let cssBar=a.querySelectorAll(`div`)[16];
-let jsBar=a.querySelectorAll(`div`)[19];
-window.addEventListener(`scroll`,function(){
-    htmlBar.classList.toggle((`barswidth1`),window.scrollY>1078);
-    cssBar.classList.toggle((`barswidth1`),window.scrollY>1138);
-    jsBar.classList.toggle((`barswidth1`),window.scrollY>1200);
-})
+let a=document.querySelector(`section`);let htmlBar=a.querySelectorAll(`div`)[13];let cssBar=a.querySelectorAll(`div`)[16];let jsBar=a.querySelectorAll(`div`)[19];
+window.addEventListener(`scroll`,function(){htmlBar.classList.toggle((`barswidth1`),window.scrollY>1078);cssBar.classList.toggle((`barswidth1`),window.scrollY>1138);jsBar.classList.toggle((`barswidth1`),window.scrollY>1200);})
+document.html.style.overflowX=(`hidden`)
+// const hoverOnCards=()=>{
+//     let sec =document.querySelector(`section`)
+//     let cards=sec.lastElementChild
+//     let card1=cards.firstElementChild;
+//   card1.style.scale=(`1.1`)
+// }
