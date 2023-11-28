7c062a93a0791444cf6a76d3417b43c7c862a4b6
DEMN9999 <demn729999@gmail.com>
2023-11-28 18:06:07 +0500

    animation to nav , header , and about section

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const hide=()=>{let btn1=document.getElementsByTagName(`div`)[0];btn1.style.display=(`none`);document.body.style.overflowY=(`auto`)};
+// const hide=()=>{let btn1=document.getElementsByTagName(`div`)[0];btn1.style.display=(`none`);document.body.style.overflowY=(`auto`)};
 window.addEventListener('scroll', function () {let nav = document.querySelector('nav');nav.classList.toggle(`stickey`, window.scrollY > 240)});
+let a=document.querySelector(`section`);
+let htmlBar=a.querySelectorAll(`div`)[13];
+let cssBar=a.querySelectorAll(`div`)[16];
+let jsBar=a.querySelectorAll(`div`)[19];
+window.addEventListener(`scroll`,function(){
+    htmlBar.classList.toggle((`barswidth1`),window.scrollY>1078);
+    cssBar.classList.toggle((`barswidth1`),window.scrollY>1138);
+    jsBar.classList.toggle((`barswidth1`),window.scrollY>1200);
+})
